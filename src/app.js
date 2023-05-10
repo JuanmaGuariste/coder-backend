05b143975ecd85ffdb15a0e159d781fd3d648024
Juanma <77707900+JuanmaGuariste@users.noreply.github.com>
2023-05-10 17:30:09 -0300

    Agregado de rutas y controladores para carritos y productos

--- src/app.js
+++ src/app.js
@@ -1,46 +1,16 @@
 import express from 'express';
-
-import ProductManager from './ProductManager.js';
+import { productsRouter } from './routers/products.router.js';
+import { cartsRouter } from './routers/carts.router.js';
 
 const app = express();
 
-const productManager = new ProductManager("./products.json");
-
-const error = {        
-    idError: "Error: el ID ingresado no existe."
-}
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/products', async (req, res) => {
-	try {
-        let limit = req.query.limit;
-		let allProducts = await productManager.getProducts();
-        if (!limit){
-            res.send(allProducts);
-        } else {
-            const totalProducts = allProducts.slice(0, limit)            
-            res.send(totalProducts);
-        }
-	} catch (err) {
-		res.send(err);
-	}
-});
-
-app.get('/products/:pid', async (req, res) => {   
-	try {
-        let id = Number(req.params.pid)
-		let product = await productManager.getProductById(id);       
-        if (!product){
-            res.send(`<p style="color: red; font-size: 30px; margin: 10px;"> ${error.idError}</p>`);
-        } else {
-            res.send(product);
-        }
-	} catch (err) {
-		res.send(err);
-	}
-});
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.listen(8080, () => {
-    console.log("Estoy escuchando el 8080")
-})
+	console.log('Server levantado en puerto 8080');
+});
