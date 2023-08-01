1dddea996399ad4f6cfe5c0fd32c9c73b115645d
Juan Manuel Guariste <juanmaguariste@gmail.com>
2023-08-01 19:07:23 -0300

    Modificaciones en creacion de tickets

--- src/controllers/tickets.controller.js
+++ src/controllers/tickets.controller.js
@@ -1,6 +1,7 @@
 import TicketsService from '../services/tickets.service.js';
 import ticketDAO from '../dao/mongo/TicketDAO.js';
 import cartsController from './carts.controller.js';
+import productsController from './products.controller.js';
 
 class TicketsController {
 	constructor() {
@@ -9,12 +10,48 @@ class TicketsController {
 
 	async getTickets() {
 		return await this.service.getTickets();
-	}
-
-	async addTicket(cid) {
-        let cart = await cartsController.getCartById(cid);
-        return cart
 	}   
+
+	async addTicket(cid, user) {
+		let productsOk = [];
+		let productsNotOk = [];
+		let totalPrice = 0;
+
+		let cart = await cartsController.getCartById(cid);
+		for (const el of cart.products) {
+			let prod = await productsController.getProductById(el.product._id);		
+			if (prod.stock - el.cant >= 0) {
+				prod.stock -= el.cant;
+				await productsController.updateProduct(prod._id, prod);
+				await cartsController.deleteProductFromCart(el.product._id, cid)				
+				let prodAuxOk = {
+					title: prod.title,
+					cant: el.cant,
+					price: prod.price
+				};
+				productsOk.push(prodAuxOk);			
+			} else {
+				let prodAuxNotOk = {
+					title: prod.title,
+					cant: el.cant,
+					stock: prod.stock,
+					price: prod.price
+				};
+				productsNotOk.push(prodAuxNotOk);	
+			}			
+		}		
+		productsOk.forEach(el => {
+			totalPrice += el.cant * el.price
+		})	
+		let ticket = {
+			//code: "3",//TODO: Generar codigo aleatorio y único
+			amount: totalPrice,
+			purchaser: user.email,
+			
+		}		
+		await this.service.addTicket(ticket);		
+		return ticket
+	}
 }
 
 const ticketsController = new TicketsController();
